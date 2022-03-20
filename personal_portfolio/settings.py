376b84d15d183b6ec97523c76ad581b1d613a66d
tanchix80 <tanchix80@gmail.com>
2022-03-20 20:53:45 +0300

    portfolio4

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -105,13 +105,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+#LANGUAGE_CODE = 'en-us'
 USE_I18N = True
+USE_L10N = False
 
-USE_TZ = True
+LANGUAGE_CODE = 'ru-RU'
+
+TIME_ZONE = 'Europe/Moscow'
+
+#USE_I18N = True
+
+#USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
